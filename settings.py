d9c40f37a7c95b96f5ece2512678525221d49458
bahoo <jon.c.culver@gmail.com>
2016-04-24 12:23:57 -0700

    Nasty html ftl.

--- settings.py
+++ settings.py
@@ -36,6 +36,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'espresso.templatetags.clean_spaces'
+            ]
         },
     },
 ]
